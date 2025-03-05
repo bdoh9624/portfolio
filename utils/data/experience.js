d82bd49206c370cab81c52109c2e658a43639d5d
bdoh9624 <159200826+bdoh9624@users.noreply.github.com>
2025-03-05 18:59:46 -0500

    Add files via upload

---
+++ utils/data/experience.js
@@ -0,0 +1,26 @@
+export const experiences = [
+  {
+    id: 1,
+    title: 'Full Stack Developer',
+    company: "Quality Systems Management, Inc.",
+    duration: "(Aug 2021 - Present)"
+  },
+  {
+    id: 2,
+    title: "FullStack Developer",
+    company: "IT Partners, Inc.",
+    duration: "(Aug 2019 - Aug 2021)"
+  },
+  {
+    id: 3,
+    title: "Software Web Developer V",
+    company: "Mystek Systems, Inc.",
+    duration: "(Oct 2018 - Aug 2019)"
+  },
+  {
+    id: 4,
+    title: "Senior Web Developer",
+    company: "General Dynamics Information Technology",
+    duration: "(Dec 2013 - Nov 2018)"
+  }
+]
---
+++ utils/data/personal-data.js
@@ -0,0 +1,19 @@
+export const personalData = {
+  name: "Brian Doherty",
+  profile: '/profile.png',
+  designation: "Full Stack Developer",
+  description: "My name is Brian Doherty. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'bd9624@gmail.com',
+  phone: '(571) 730-7128',
+  address: 'McLean, Virginia - 22102 ',
+  github: 'https://github.com/bdoh9624/portfolio',
+  // github: 'https://github.com/said7388',
+  facebook: 'https://www.facebook.com/abusaid.riyaz/',
+  linkedIn: 'https://www.linkedin.com/in/brian-doherty-3a822410/',
+  // linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
+  twitter: 'https://twitter.com/said7388',
+  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+  leetcode: "https://leetcode.com/said3812/",
+  devUsername: "said7388",
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+}
